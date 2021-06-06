afe71cba35c850836225cecc88d34e499e0b860f
BaptistaNeves <baptistafirminoneves@gmail.com>
2021-06-06 16:02:22 +0100

    Updating UsersController

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -10,7 +10,7 @@ namespace API.Controllers
 {
     [ApiController]
 
-    
+
     [Route("api/[controller]")]
     public class UsersController : ControllerBase
     {
@@ -27,7 +27,7 @@ namespace API.Controllers
             return await _context.Users.ToListAsync();
         }
 
-        [HttpGet("{id}")]
+        [HttpGet("{id:int}")]
         public async Task<ActionResult<AppUser>> GetUser(int id)
         {
             return await _context.Users.FindAsync(id);
